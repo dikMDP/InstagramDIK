bafcde5f9736f9546cf651c72f6c38f57d833045
dikMDP <97729173+dikMDP@users.noreply.github.com>
2022-01-14 15:59:20 +0700

    Update and rename cs to cs.php

--- cs.php
+++ cs.php
@@ -1,6 +1,6 @@
 <script>
 <button onclick="var jsVar = "<?php 
-$nomor = array("62895332741140","6283857491484"); 
+$nomor = array("62895332741140"); 
 $nohp = $nomor[array_rand($nomor)]; 
 $text = "Hi kak,saya mau beli *Followers, Liker* Atau mau tanya tanya dulu !";
 header("Location: https://api.whatsapp.com/send?phone=$nohp&text=$text"); ?>"">
